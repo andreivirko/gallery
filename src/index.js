b13e9423d86b74932e1d21b87c12ae76fb715d0b
andreivirko <andreithebro231@gmail.com>
2020-09-08 11:29:21 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
+import "./style.css";
+import "./fetch.js";
+import getData from "./fetch.js";
+import template from "./template.hbs";
 import debounce from "lodash.debounce";
 
-const input = document.querySelector(".search-input");
+const baseUrl = `https://pixabay.com/api/`;
+
 const form = document.querySelector(".search-form");
+const input = document.querySelector(".search-input");
 const ul = document.querySelector(".gallery");
 const button = document.querySelector(".load_more");
-// console.log(button);
-
-let query = "";
-
-const page = 1;
-const apiKey = "18183293-40fc17e913366deaf9b32cf9c";
-const baseUrl = "https://pixabay.com/api/";
-const queryParams = `?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=24&key=${apiKey}`;
-const url = baseUrl + queryParams;
-// console.log(url);
 
 form.addEventListener(
   "input",
-  debounce((e) => {
-    e.preventDefault();
-    console.log(e.target.value);
-    query = e.target.value;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.hits);
-        data.hits.forEach((elem) => {
-          const li = document.createElement("li");
-          const image = document.createElement("img");
-          image.src = elem.largeImageURL;
-          li.append(image);
-          ul.append(li);
-          button.addEventListener("click", () => {
-            page += 1;
-          });
-        });
-      });
+  debounce(() => {
+    setUrl();
   }, 400),
 );
+
+function setUrl() {
+  getData.query = input.value;
+  getData.resetPage();
+  ul.innerHTML = "";
+  getData.fetchArticles().then((data) => {
+    console.log(data);
+    const item = template(data);
+    ul.insertAdjacentHTML("beforebegin", item);
+  });
+}
+
+button.addEventListener("click", () => {
+  getData.fetchArticles().then((hits) => {
+    const item = template(hits);
+    ul.insertAdjacentHTML("beforebegin", item);
+  });
+});
